b0e9b007681d87ea5a37af0f1a1d5db1b15f0673
Neosega <neoknip@gmail.com>
2025-02-15 20:15:20 -0500

    ExpressJS and Typescript boilerplate files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# todo-server
+# Todo Server
 Backend services for todo application
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("To Do Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
